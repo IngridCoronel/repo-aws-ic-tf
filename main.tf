13cb562555950c866f0ffb49b5040950edf52219
Ingrid Coronel <ingridcoronelc@gmail.com>
2024-01-24 18:30:06 -0600

    Add depends on subs sns

--- main.tf
+++ main.tf
@@ -6,4 +6,8 @@ resource "aws_sns_topic_subscription" "sns_subs_email" {
   topic_arn = aws_sns_topic.sns_topic.arn
   protocol  = "email"
   endpoint  = "ingridcoronelc@gmail.com"
+
+  depends_on = [
+    aws_sns_topic.sns_topic
+  ]  
 }
