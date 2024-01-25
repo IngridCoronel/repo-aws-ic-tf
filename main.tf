427ee446e8d971773c8d817666f6c6246e1e8234
Ingrid Coronel <ingridcoronelc@gmail.com>
2024-01-24 18:19:47 -0600

    Add sns subscription

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 resource "aws_sns_topic" "sns_topic" {
   name = "topic-msg-sf-aws"
 }
+
+resource "aws_sns_topic_subscription" "sns_subs_email" {
+  topic_arn = aws_sns_topic.sns_topic.arn
+  protocol  = "email"
+  endpoint  = "ingridcoronelc@gmail.com"
+}
