d26738aaa2d53aca05388e905688647167a5853f
Ingrid Coronel <ingridcoronelc@gmail.com>
2024-01-24 18:25:38 -0600

    Remove depends on susbs sns

--- main.tf
+++ main.tf
@@ -3,8 +3,6 @@ resource "aws_sns_topic" "sns_topic" {
 }
 
 resource "aws_sns_topic_subscription" "sns_subs_email" {
-  depends_on  = aws_sns_topic.sns_topic
-
   topic_arn = aws_sns_topic.sns_topic.arn
   protocol  = "email"
   endpoint  = "ingridcoronelc@gmail.com"
